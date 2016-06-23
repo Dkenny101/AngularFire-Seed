3559859aff1539a62d598012ecbb478c7eac70f0
Darragh Kenny <Kenny@Darraghs-MacBook-Pro.local>
2016-06-23 16:25:47 +0100

    Initial Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,9 @@
+/**
+ * @ngdoc overview
+ * @name AngularFire Seed
+ * @description
+ * # AngularFire Seed App
+ *
+ * Main module of the application.
+ */
+var app = angular.module('angularFireSeedApp', ['firebase']);
